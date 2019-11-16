92ae145b54e48c7640d02a96e06610f9880864a8
MOHINI TRIPATHI <trishmohi01@gmail.com>
2019-11-16 23:34:49 +0530

    name

--- test/armstrong_number/core_test.clj
+++ test/armstrong_number/core_test.clj
@@ -6,23 +6,23 @@
 
 (deftest test-I
   (testing "Is 5 an Armstrong number?"
-    (is (narcissitstic? 5))))
+    (is (narcissistic? 5))))
 
 (deftest test-II
   (testing "Is 153 an Armstrong number?"
-    (is (narcissitstic? 153))))
+    (is (narcissistic? 153))))
 
 (deftest test-III
   (testing "Is 100 not an Armstrong number?")
-  (is (not (narcissitstic? 100))))
+  (is (not (narcissistic? 100))))
 
 (deftest test-IV
   (testing "Is 9922889 an Armstrong number?")
-  (is (narcissitstic? 9922889)))
+  (is (narcissistic? 9922889)))
 
 (deftest test-V
   (testing "Is 7607881808 an Armstrong number?")
-  (is (narcissitstic? 7607881808)))
+  (is (narcissistic? 7607881808)))
 
 
 (run-all-tests)
